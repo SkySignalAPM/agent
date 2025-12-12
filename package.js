19c9857d824e98f610ce2d72cb7f931013083805
Michael Vogt <mtpfeif@gmail.com>
2025-12-11 22:45:04 -0600

    collection stats

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "skysignal:agent",
-  version: "1.0.4",
+  version: "1.0.5",
   summary:
     "SkySignal APM agent for Meteor applications - monitors performance, errors, and system metrics",
   git: "https://github.com/skysignalapm/agent.git",
